72a2811ae7e9fe36b9ffee149150266fc8e15159
vibhorarya12 <vibhorarya12@gmail.com>
2024-04-01 14:22:18 +0530

    added stats buttons

--- Screens/DashboardScreens/MonthlyStats.js
+++ Screens/DashboardScreens/MonthlyStats.js
@@ -1,59 +1,124 @@
-import React, {  useState } from "react";
-import { StyleSheet, View, Dimensions, Button , Text} from "react-native";
-import {LineChart}  from "react-native-chart-kit";
+import React from "react";
+import { StyleSheet, View, Dimensions,  Text ,TouchableOpacity } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+import { LinearGradient } from "expo-linear-gradient";
 const { width, height } = Dimensions.get("window");
 export default function MonthlyStatsScreen() {
-  
   return (
     <View style={styles.container}>
-       <View style={styles.GraphContainer}>
-       <LineChart
-    data={{
-      labels: ["January", "February", "March", "April", "May", "June"],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100
-          ]
-        }
-      ]
-    }}
-    width={Dimensions.get("window").width} // from react-native
-    height={220}
-    yAxisLabel="$"
-    yAxisSuffix="k"
-    yAxisInterval={1} // optional, defaults to 1
-    chartConfig={{
-      backgroundColor: "#e26a00",
-      backgroundGradientFrom: "#fb8c00",
-      backgroundGradientTo: "#ffa726",
-      decimalPlaces: 2, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-      propsForDots: {
-        r: "6",
-        strokeWidth: "2",
-        stroke: "#ffa726"
-      }
-    }}
-    bezier
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  />
-       </View>
-      <View style={styles.StatsContainer}>
-        
+      <View style={styles.GraphContainer}>
+      <View style={styles.GraphBox}>
+        <LineChart
+          data={{
+            labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"], // x axis
+            datasets: [
+              {
+                data: [5, 10, 17, 14, 7, 20, 35],
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width * 0.98} // from react-native
+          height={width * 0.61}
+          yAxisSuffix="-stps"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            // backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#540D6E",
+            backgroundGradientTo: "#540D6E",
+            decimalPlaces: 0, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 2,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "red",
+            },
+          }}
+          withVerticalLines={false}
+          withHorizontalLines={false}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 10,
+          }}
+        />
+        </View>
+        <View style={styles.SelectorBox}>
+        <TouchableOpacity
+            style={{ backgroundColor: "transparent" }}
+           
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              colors={["#eb3349", "#f45c43"]}
+              style={styles.button}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: width * 0.04,
+                  textShadowColor: "rgba(0, 0, 0, 0.8)",
+                  textShadowOffset: { width: 3, height: 2 },
+                  textShadowRadius: 6,
+                }}
+              >
+                Day
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        <TouchableOpacity
+            style={{ backgroundColor: "transparent" }}
+           
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              colors={["#eb3349", "#f45c43"]}
+              style={styles.button}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: width * 0.04,
+                  textShadowColor: "rgba(0, 0, 0, 0.8)",
+                  textShadowOffset: { width: 3, height: 2 },
+                  textShadowRadius: 6,
+                }}
+              >
+               Week
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        <TouchableOpacity
+            style={{ backgroundColor: "transparent" }}
+           
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              colors={["#eb3349", "#f45c43"]}
+              style={styles.button}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: width * 0.04,
+                  textShadowColor: "rgba(0, 0, 0, 0.8)",
+                  textShadowOffset: { width: 3, height: 2 },
+                  textShadowRadius: 6,
+                }}
+              >
+               Month
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
       </View>
+      <View style={styles.StatsContainer}></View>
     </View>
   );
 }
@@ -67,15 +132,48 @@ const styles = StyleSheet.create({
   },
   GraphContainer: {
     borderColor: "black",
-    borderWidth: 2,
+    // borderWidth: 2,
     flex: 5,
     marginTop: width * 0.15,
     justifyContent: "center",
     alignItems: "center",
+    display:'flex',
+    flexDirection:'column'
+    
+  },
+  GraphBox:{
+    flex:8,
+    display:'flex',
+    // borderWidth:2,
+    borderColor:'black',
+    justifyContent:'flex-end'
+   
+  },
+  SelectorBox:{
+    flex:2,
+    width:width*0.97,
+    
+    borderColor:'black',
+    display:'flex',
+    flexDirection:'row',
+    gap:10,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:25,
+    backgroundColor:'#D3C5E5',
+    elevation:5
+  },
+  button: {
+    width: width * 0.27,
+    justifyContent: "center",
+    alignItems: "center",
+    height: height * 0.05,
+    borderRadius: 25,
+    
   },
   StatsContainer: {
     borderColor: "black",
-    borderWidth: 2,
+    // borderWidth: 2,
     flex: 5,
   },
 });
